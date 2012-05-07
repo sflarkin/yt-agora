54afe2abde5c497f05f747f2054e432497245bfe
Matthew Turk <matthewturk@gmail.com>
2012-05-07 10:17:08 -0400

    Change parallel_profile to report the size in the filename, and also to yield the filename it uses.

---
+++ yt/utilities/answer_testing/__init__.py
@@ -0,0 +1,31 @@
+"""
+The components of the Enzo testing mechanism
+
+Author: Matthew Turk <matthewturk@gmail.com>
+Affiliation: UCSD
+Homepage: http://yt-project.org/
+License:
+  Copyright (C) 2010-2011 Matthew Turk.  All Rights Reserved.
+
+  This file is part of yt.
+
+  yt is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import runner
+import output_tests
+from runner import RegressionTestRunner
+
+from output_tests import RegressionTest, SingleOutputTest, \
+    MultipleOutputTest, YTStaticOutputTest, create_test
