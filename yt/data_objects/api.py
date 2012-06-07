b062e2678d2b32b58e676f89eb22a4bdb017ef80
Matthew Turk <matthewturk@gmail.com>
2012-06-07 14:58:12 -0400

    Removing pylab import

---
+++ yt/data_objects/api.py
@@ -0,0 +1,87 @@
+"""
+API for yt.data_objects
+
+Author: Matthew Turk <matthewturk@gmail.com>
+Affiliation: UCSD
+Author: J.S. Oishi <jsoishi@gmail.com>
+Affiliation: KIPAC/SLAC/Stanford
+Author: Britton Smith <brittonsmith@gmail.com>
+Affiliation: MSU
+Homepage: http://yt-project.org/
+License:
+  Copyright (C) 2010-2011 Matthew Turk.  All Rights Reserved.
+
+  This file is part of yt.
+
+  yt is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from grid_patch import \
+    AMRGridPatch
+
+from hierarchy import \
+    AMRHierarchy
+
+from static_output import \
+    StaticOutput
+
+from object_finding_mixin import \
+    ObjectFindingMixin
+
+from particle_io import \
+    ParticleIOHandler, \
+    particle_handler_registry
+
+from profiles import \
+    EmptyProfileData, \
+    BinnedProfile, \
+    BinnedProfile1D, \
+    BinnedProfile2D, \
+    BinnedProfile3D
+
+from time_series import \
+    TimeSeriesData, \
+    TimeSeriesDataObject
+
+from analyzer_objects import \
+    AnalysisTask, analysis_task
+
+from data_containers import \
+    data_object_registry
+
+from derived_quantities import \
+    quantity_info, \
+    add_quantity
+
+from field_info_container import \
+    FieldInfoContainer, \
+    FieldInfo, \
+    NeedsGridType, \
+    NeedsOriginalGrid, \
+    NeedsDataField, \
+    NeedsProperty, \
+    NeedsParameter, \
+    FieldDetector, \
+    DerivedField, \
+    ValidateParameter, \
+    ValidateDataField, \
+    ValidateProperty, \
+    ValidateSpatial, \
+    ValidateGridType, \
+    add_field, \
+    derived_field
+
+from particle_trajectories import \
+    ParticleTrajectoryCollection
