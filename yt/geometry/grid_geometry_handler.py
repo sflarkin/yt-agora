750e5bf8f419d299cc9d7943b2dd688114164f44
Matthew Turk <matthewturk@gmail.com>
2013-08-26 10:07:35 -0400

    Adding "halo_analysis" analysis module.

---
+++ yt/geometry/grid_geometry_handler.py
@@ -0,0 +1,291 @@
+"""
+AMR hierarchy container class
+
+Author: Matthew Turk <matthewturk@gmail.com>
+Affiliation: KIPAC/SLAC/Stanford
+Homepage: http://yt-project.org/
+License:
+  Copyright (C) 2007-2011 Matthew Turk.  All Rights Reserved.
+
+  This file is part of yt.
+
+  yt is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import h5py
+import numpy as np
+import string, re, gc, time, cPickle
+import weakref
+
+from itertools import chain, izip
+
+from yt.funcs import *
+from yt.utilities.logger import ytLogger as mylog
+from yt.arraytypes import blankRecordArray
+from yt.config import ytcfg
+from yt.data_objects.field_info_container import NullFunc
+from yt.geometry.geometry_handler import \
+    GeometryHandler, YTDataChunk, ChunkDataCache
+from yt.utilities.definitions import MAXLEVEL
+from yt.utilities.physical_constants import sec_per_year
+from yt.utilities.io_handler import io_registry
+from yt.utilities.parallel_tools.parallel_analysis_interface import \
+    ParallelAnalysisInterface, parallel_splitter
+from yt.utilities.lib import GridTree, MatchPointsToGrids
+
+from yt.data_objects.data_containers import data_object_registry
+
+class GridGeometryHandler(GeometryHandler):
+    float_type = 'float64'
+    _preload_implemented = False
+
+    def _setup_geometry(self):
+        mylog.debug("Counting grids.")
+        self._count_grids()
+
+        mylog.debug("Initializing grid arrays.")
+        self._initialize_grid_arrays()
+
+        mylog.debug("Parsing hierarchy.")
+        self._parse_hierarchy()
+
+        mylog.debug("Constructing grid objects.")
+        self._populate_grid_objects()
+
+        mylog.debug("Re-examining hierarchy")
+        self._initialize_level_stats()
+
+    @property
+    def parameters(self):
+        return self.parameter_file.parameters
+
+    def select_grids(self, level):
+        """
+        Returns an array of grids at *level*.
+        """
+        return self.grids[self.grid_levels.flat == level]
+
+    def get_levels(self):
+        for level in range(self.max_level+1):
+            yield self.select_grids(level)
+
+    def _initialize_grid_arrays(self):
+        mylog.debug("Allocating arrays for %s grids", self.num_grids)
+        self.grid_dimensions = np.ones((self.num_grids,3), 'int32')
+        self.grid_left_edge = np.zeros((self.num_grids,3), self.float_type)
+        self.grid_right_edge = np.ones((self.num_grids,3), self.float_type)
+        self.grid_levels = np.zeros((self.num_grids,1), 'int32')
+        self.grid_particle_count = np.zeros((self.num_grids,1), 'int32')
+
+    def clear_all_data(self):
+        """
+        This routine clears all the data currently being held onto by the grids
+        and the data io handler.
+        """
+        for g in self.grids: g.clear_data()
+        self.io.queue.clear()
+
+    def get_smallest_dx(self):
+        """
+        Returns (in code units) the smallest cell size in the simulation.
+        """
+        return self.select_grids(self.grid_levels.max())[0].dds[:].min()
+
+    def _initialize_level_stats(self):
+        # Now some statistics:
+        #   0 = number of grids
+        #   1 = number of cells
+        #   2 = blank
+        desc = {'names': ['numgrids','numcells','level'],
+                'formats':['Int64']*3}
+        self.level_stats = blankRecordArray(desc, MAXLEVEL)
+        self.level_stats['level'] = [i for i in range(MAXLEVEL)]
+        self.level_stats['numgrids'] = [0 for i in range(MAXLEVEL)]
+        self.level_stats['numcells'] = [0 for i in range(MAXLEVEL)]
+        for level in xrange(self.max_level+1):
+            self.level_stats[level]['numgrids'] = np.sum(self.grid_levels == level)
+            li = (self.grid_levels[:,0] == level)
+            self.level_stats[level]['numcells'] = self.grid_dimensions[li,:].prod(axis=1).sum()
+
+    @property
+    def grid_corners(self):
+        return np.array([
+          [self.grid_left_edge[:,0], self.grid_left_edge[:,1], self.grid_left_edge[:,2]],
+          [self.grid_right_edge[:,0], self.grid_left_edge[:,1], self.grid_left_edge[:,2]],
+          [self.grid_right_edge[:,0], self.grid_right_edge[:,1], self.grid_left_edge[:,2]],
+          [self.grid_left_edge[:,0], self.grid_right_edge[:,1], self.grid_left_edge[:,2]],
+          [self.grid_left_edge[:,0], self.grid_left_edge[:,1], self.grid_right_edge[:,2]],
+          [self.grid_right_edge[:,0], self.grid_left_edge[:,1], self.grid_right_edge[:,2]],
+          [self.grid_right_edge[:,0], self.grid_right_edge[:,1], self.grid_right_edge[:,2]],
+          [self.grid_left_edge[:,0], self.grid_right_edge[:,1], self.grid_right_edge[:,2]],
+        ], dtype='float64')
+
+    def print_stats(self):
+        """
+        Prints out (stdout) relevant information about the simulation
+        """
+        header = "%3s\t%6s\t%14s\t%14s" % ("level","# grids", "# cells",
+                                           "# cells^3")
+        print header
+        print "%s" % (len(header.expandtabs())*"-")
+        for level in xrange(MAXLEVEL):
+            if (self.level_stats['numgrids'][level]) == 0:
+                break
+            print "% 3i\t% 6i\t% 14i\t% 14i" % \
+                  (level, self.level_stats['numgrids'][level],
+                   self.level_stats['numcells'][level],
+                   self.level_stats['numcells'][level]**(1./3))
+            dx = self.select_grids(level)[0].dds[0]
+        print "-" * 46
+        print "   \t% 6i\t% 14i" % (self.level_stats['numgrids'].sum(), self.level_stats['numcells'].sum())
+        print "\n"
+        try:
+            print "z = %0.8f" % (self["CosmologyCurrentRedshift"])
+        except:
+            pass
+        t_s = self.pf.current_time * self.pf["Time"]
+        print "t = %0.8e = %0.8e s = %0.8e years" % \
+            (self.pf.current_time, \
+             t_s, t_s / sec_per_year )
+        print "\nSmallest Cell:"
+        u=[]
+        for item in self.parameter_file.units.items():
+            u.append((item[1],item[0]))
+        u.sort()
+        for unit in u:
+            print "\tWidth: %0.3e %s" % (dx*unit[0], unit[1])
+
+    def find_max(self, field, finest_levels = 3):
+        """
+        Returns (value, center) of location of maximum for a given field.
+        """
+        if (field, finest_levels) in self._max_locations:
+            return self._max_locations[(field, finest_levels)]
+        mv, pos = self.find_max_cell_location(field, finest_levels)
+        self._max_locations[(field, finest_levels)] = (mv, pos)
+        return mv, pos
+
+    def find_max_cell_location(self, field, finest_levels = 3):
+        if finest_levels is not False:
+            gi = (self.grid_levels >= self.max_level - finest_levels).ravel()
+            source = self.data_collection([0.0]*3, self.grids[gi])
+        else:
+            source = self.all_data()
+        mylog.debug("Searching for maximum value of %s", field)
+        max_val, maxi, mx, my, mz = \
+            source.quantities["MaxLocation"](field)
+        mylog.info("Max Value is %0.5e at %0.16f %0.16f %0.16f", 
+              max_val, mx, my, mz)
+        self.parameters["Max%sValue" % (field)] = max_val
+        self.parameters["Max%sPos" % (field)] = "%s" % ((mx,my,mz),)
+        return max_val, np.array((mx,my,mz), dtype='float64')
+
+    def find_points(self, x, y, z) :
+        """
+        Returns the (objects, indices) of leaf grids containing a number of (x,y,z) points
+        """
+        x = ensure_numpy_array(x)
+        y = ensure_numpy_array(y)
+        z = ensure_numpy_array(z)
+        if not len(x) == len(y) == len(z):
+            raise AssertionError("Arrays of indices must be of the same size")
+
+        grid_tree = self.get_grid_tree()
+        pts = MatchPointsToGrids(grid_tree, len(x), x, y, z)
+        ind = pts.find_points_in_tree()
+        return self.grids[ind], ind
+
+    def get_grid_tree(self) :
+
+        left_edge = np.zeros((self.num_grids, 3))
+        right_edge = np.zeros((self.num_grids, 3))
+        level = np.zeros((self.num_grids), dtype='int64')
+        parent_ind = np.zeros((self.num_grids), dtype='int64')
+        num_children = np.zeros((self.num_grids), dtype='int64')
+
+        for i, grid in enumerate(self.grids) :
+
+            left_edge[i,:] = grid.LeftEdge
+            right_edge[i,:] = grid.RightEdge
+            level[i] = grid.Level
+            if grid.Parent is None :
+                parent_ind[i] = -1
+            else :
+                parent_ind[i] = grid.Parent.id - grid.Parent._id_offset
+            num_children[i] = np.int64(len(grid.Children))
+
+        return GridTree(self.num_grids, left_edge, right_edge, parent_ind,
+                        level, num_children)
+
+    def convert(self, unit):
+        return self.parameter_file.conversion_factors[unit]
+
+    def _identify_base_chunk(self, dobj):
+        if dobj._type_name == "grid":
+            dobj._chunk_info = np.empty(1, dtype='object')
+            dobj._chunk_info[0] = dobj
+        elif getattr(dobj, "_grids", None) is None:
+            gi = dobj.selector.select_grids(self.grid_left_edge,
+                                            self.grid_right_edge,
+                                            self.grid_levels)
+            grids = list(sorted(self.grids[gi], key = lambda g: g.filename))
+            dobj._chunk_info = np.empty(len(grids), dtype='object')
+            for i, g in enumerate(grids):
+                dobj._chunk_info[i] = g
+        if getattr(dobj, "size", None) is None:
+            dobj.size = self._count_selection(dobj)
+        if getattr(dobj, "shape", None) is None:
+            dobj.shape = (dobj.size,)
+        dobj._current_chunk = list(self._chunk_all(dobj, cache = False))[0]
+
+    def _count_selection(self, dobj, grids = None):
+        if grids is None: grids = dobj._chunk_info
+        count = sum((g.count(dobj.selector) for g in grids))
+        return count
+
+    def _chunk_all(self, dobj, cache = True):
+        gobjs = getattr(dobj._current_chunk, "objs", dobj._chunk_info)
+        yield YTDataChunk(dobj, "all", gobjs, dobj.size, cache)
+        
+    def _chunk_spatial(self, dobj, ngz, sort = None, preload_fields = None):
+        gobjs = getattr(dobj._current_chunk, "objs", dobj._chunk_info)
+        if sort in ("+level", "level"):
+            giter = sorted(gobjs, key = g.Level)
+        elif sort == "-level":
+            giter = sorted(gobjs, key = -g.Level)
+        elif sort is None:
+            giter = gobjs
+        if self._preload_implemented and preload_fields is not None and ngz == 0:
+            giter = ChunkDataCache(list(giter), preload_fields, self)
+        for i, og in enumerate(giter):
+            if ngz > 0:
+                g = og.retrieve_ghost_zones(ngz, [], smoothed=True)
+            else:
+                g = og
+            size = self._count_selection(dobj, [og])
+            if size == 0: continue
+            # We don't want to cache any of the masks or icoords or fcoords for
+            # individual grids.
+            yield YTDataChunk(dobj, "spatial", [g], size, cache = False)
+
+    def _chunk_io(self, dobj, cache = True):
+        gfiles = defaultdict(list)
+        gobjs = getattr(dobj._current_chunk, "objs", dobj._chunk_info)
+        for g in gobjs:
+            gfiles[g.filename].append(g)
+        for fn in sorted(gfiles):
+            gs = gfiles[fn]
+            yield YTDataChunk(dobj, "io", gs, self._count_selection(dobj, gs),
+                              cache = cache)
+
