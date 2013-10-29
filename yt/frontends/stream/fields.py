034b3072649cb1433b4b8e47a33e2d4e4ca632ee
Nathan Goldbaum <goldbaum@ucolick.org>
2013-10-29 15:00:07 -0700

    Fixing a unit conversion issue with the arrow callback.

---
+++ yt/frontends/stream/fields.py
@@ -0,0 +1,33 @@
+"""
+Fields specific to Streaming data
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from yt.data_objects.field_info_container import \
+    FieldInfoContainer, \
+    NullFunc, \
+    TranslationFunc, \
+    FieldInfo, \
+    ValidateParameter, \
+    ValidateDataField, \
+    ValidateProperty, \
+    ValidateSpatial, \
+    ValidateGridType
+import yt.data_objects.universal_fields
+
+KnownStreamFields = FieldInfoContainer()
+add_stream_field = KnownStreamFields.add_field
+
+StreamFieldInfo = FieldInfoContainer.create_with_fallback(FieldInfo)
+add_field = StreamFieldInfo.add_field
+
