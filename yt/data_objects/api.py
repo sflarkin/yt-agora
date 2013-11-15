22676c09d6431101b45f3187668108120d3b7cf5
Matthew Turk <matthewturk@gmail.com>
2013-11-15 14:48:39 -0500

    This should eliminate segfaults for particles that are OOB.

---
+++ yt/data_objects/api.py
@@ -0,0 +1,79 @@
+""" 
+API for yt.data_objects
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from grid_patch import \
+    AMRGridPatch
+
+from octree_subset import \
+    OctreeSubset
+
+from static_output import \
+    StaticOutput
+
+from particle_io import \
+    ParticleIOHandler, \
+    particle_handler_registry
+
+from profiles import \
+    EmptyProfileData, \
+    BinnedProfile, \
+    BinnedProfile1D, \
+    BinnedProfile2D, \
+    BinnedProfile3D
+
+from time_series import \
+    TimeSeriesData, \
+    TimeSeriesDataObject
+
+from analyzer_objects import \
+    AnalysisTask, analysis_task
+
+from data_containers import \
+    data_object_registry
+
+import construction_data_containers as __cdc
+import selection_data_containers as __sdc
+
+from derived_quantities import \
+    quantity_info, \
+    add_quantity
+
+from image_array import \
+    ImageArray
+
+from field_info_container import \
+    FieldInfoContainer, \
+    FieldInfo, \
+    NeedsGridType, \
+    NeedsOriginalGrid, \
+    NeedsDataField, \
+    NeedsProperty, \
+    NeedsParameter, \
+    FieldDetector, \
+    DerivedField, \
+    ValidateParameter, \
+    ValidateDataField, \
+    ValidateProperty, \
+    ValidateSpatial, \
+    ValidateGridType, \
+    add_field, \
+    add_grad, \
+    derived_field
+
+from particle_trajectories import \
+    ParticleTrajectoryCollection
+
+from particle_filters import \
+    particle_filter
