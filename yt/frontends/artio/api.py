fe8f36d02150018f93b77c5e8db131a3a856582a
bcrosby <bcrosby.bd@gmail.com>
2014-01-21 15:02:17 -0500

    Trying to sort out why particle masses are not converted properly when using the Enzo frontend for inline analysis

---
+++ yt/frontends/artio/api.py
@@ -0,0 +1,25 @@
+"""
+API for yt.frontends.artio
+
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from .data_structures import \
+    ARTIOStaticOutput
+
+from .fields import \
+    ARTIOFieldInfo, \
+    add_artio_field
+
+from .io import \
+    IOHandlerARTIO
---
+++ yt/frontends/stream/fields.py
@@ -0,0 +1,53 @@
+"""
+Fields specific to Streaming data
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from yt.data_objects.field_info_container import \
+    FieldInfoContainer, \
+    NullFunc, \
+    TranslationFunc, \
+    FieldInfo, \
+    ValidateParameter, \
+    ValidateDataField, \
+    ValidateProperty, \
+    ValidateSpatial, \
+    ValidateGridType
+import yt.fields.universal_fields
+from yt.fields.particle_fields import \
+    particle_deposition_functions, \
+    particle_vector_functions
+
+KnownStreamFields = FieldInfoContainer()
+add_stream_field = KnownStreamFields.add_field
+
+StreamFieldInfo = FieldInfoContainer.create_with_fallback(FieldInfo)
+add_field = StreamFieldInfo.add_field
+
+add_stream_field("Density", function = NullFunc)
+add_stream_field("x-velocity", function = NullFunc)
+add_stream_field("y-velocity", function = NullFunc)
+add_stream_field("z-velocity", function = NullFunc)
+
+def _setup_particle_fields(registry, ptype):
+    for fn in ["creation_time", "dynamical_time", "metallicity_fraction"] + \
+              ["particle_type", "particle_index", "particle_mass"] + \
+              ["particle_position_%s" % ax for ax in 'xyz'] + \
+              ["particle_velocity_%s" % ax for ax in 'xyz']:
+        registry.add_field((ptype, fn), function=NullFunc, particle_type=True)
+    particle_vector_functions(ptype,
+        ["particle_position_%s" % ax for ax in 'xyz'],
+        ["particle_velocity_%s" % ax for ax in 'xyz'],
+        registry)
+    particle_deposition_functions(ptype,
+        "Coordinates", "particle_mass", registry)
