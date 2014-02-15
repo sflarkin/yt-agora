c9fb537f124b8c37a677f66d4bea990cf3c1aa41
Matthew Turk <matthewturk@gmail.com>
2014-02-15 13:47:21 -0500

    Adding ORIGAMI routines from Bridget Falck.

---
+++ yt/frontends/stream/api.py
@@ -0,0 +1,32 @@
+"""
+API for yt.frontends.stream
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from .data_structures import \
+      StreamGrid, \
+      StreamHierarchy, \
+      StreamStaticOutput, \
+      StreamHandler, \
+      load_uniform_grid, \
+      load_amr_grids, \
+      load_particles, \
+      load_hexahedral_mesh, \
+      load_octree, \
+      refine_amr
+
+from .fields import \
+      StreamFieldInfo
+
+from .io import \
+      IOHandlerStream
