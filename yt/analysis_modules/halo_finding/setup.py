02a547b8c6987a7298e9901fba9a0979bb42a253
Andrew Myers <atmyers2@gmail.com>
2014-06-06 14:37:47 -0700

    Depending on the type of output function called from Chombo, "max_level" may not exist in the data dump. "num_levels" will, however.

---
+++ yt/analysis_modules/halo_finding/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os.path
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('halo_finding', parent_package, top_path)
+    config.add_subpackage("fof")
+    config.add_subpackage("hop")
+    config.add_subpackage("parallel_hop")
+    if os.path.exists("rockstar.cfg"):
+        config.add_subpackage("rockstar")
+    config.make_config_py()  # installs __config__.py
+    return config
