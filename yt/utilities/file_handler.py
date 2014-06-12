97ad781a896a6787a61243e30396cf6790a7b886
Nathan Goldbaum <goldbaum@ucolick.org>
2014-06-11 22:19:27 -0700

    Making FileHandler a bit fancier to avoid rewriting a lot of code.

---
+++ yt/utilities/file_handler.py
@@ -0,0 +1,29 @@
+"""
+A wrapper class for h5py file objects.
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import h5py
+
+class FileHandler(object):
+    def __init__(self, filename):
+        self.handle = h5py.File(filename, 'r')
+
+    def __del__(self):
+        self.handle.close()
+
+    def __getitem__(self, key):
+        return self.handle[key]
+
+    def __contains__(self, item):
+        return item in self.handle
