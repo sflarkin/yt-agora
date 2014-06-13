7aae7ce3ac7ac0cfebe0f2a1566390c00f46781b
Matthew Turk <matthewturk@gmail.com>
2014-06-13 08:14:15 -0600

    Remove python-level domain checks, move them into compute_morton.

---
+++ doc/source/cookbook/average_value.py
@@ -0,0 +1,13 @@
+import yt
+
+ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")  # load data
+
+field = "temperature"  # The field to average
+weight = "cell_mass"  # The weight for the average
+
+dd = ds.h.all_data()  # This is a region describing the entire box,
+                      # but note it doesn't read anything in yet!
+# We now use our 'quantities' call to get the average quantity
+average_value = dd.quantities["WeightedAverageQuantity"](field, weight)
+
+print "Average %s (weighted by %s) is %0.5e" % (field, weight, average_value)
