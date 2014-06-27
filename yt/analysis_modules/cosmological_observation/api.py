9beb49151d7cf4884e95e8f770f6099ce84813c3
Hilary Egan <hilaryye@gmail.com>
2014-06-27 12:06:23 -0600

    Make annotate particles work with ptype

---
+++ yt/analysis_modules/cosmological_observation/api.py
@@ -0,0 +1,25 @@
+"""
+API for cosmology analysis.
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from .cosmology_splice import \
+    CosmologySplice
+
+from .light_cone.api import \
+    LightCone, \
+    find_unique_solutions, \
+    project_unique_light_cones
+
+from .light_ray.api import \
+    LightRay
