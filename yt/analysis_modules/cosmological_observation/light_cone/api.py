d126d9fd68735a65c0240635e276a1cfe49daa02
John ZuHone <jzuhone@gmail.com>
2014-07-17 23:24:22 -0400

    Move this inside the try...except to avoid errors on Windows.

---
+++ yt/analysis_modules/cosmological_observation/light_cone/api.py
@@ -0,0 +1,21 @@
+"""
+API for lightcone
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from .light_cone import \
+    LightCone
+
+from .unique_solution import \
+    project_unique_light_cones, \
+    find_unique_solutions
